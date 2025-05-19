6cf928560a68522c1e624de11199546b49e2c0ca
archiseino <archiseino@gmail.com>
2025-05-19 11:02:39 +0700

    update: dataset update

---
+++ UBFC-Phys/methods/__init__.py
@@ -0,0 +1,6 @@
+## Import all methods
+from methods.POS import POS_WANG
+from methods.LGI import LGI
+from methods.GREEN import GREEN
+from methods.CHROM import CHROME_DEHAAN
+from methods.OMIT import OMIT
